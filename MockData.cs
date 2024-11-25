325ddbdc28afbcaf52ae63bd944b47285730c0e3
phillip deleon <phillipdeleon68@gmail.com>
2024-11-25 17:51:49 -0500

    Add project files.

---
+++ MockData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+
+namespace Assignment_4._1
+{
+    internal class MockData
+    {
+        public static List<Student> Students;
+
+        public static List<Student> CreateData()
+        {
+            Students = new List<Student>();
+            {
+                new Student() { StudentId = 1, FirstName = "Kevin", LastName = "Billy", Address = "165 Hickory", Grade = 'B'};
+                new Student() { StudentId = 2, FirstName = "Phill", LastName = "Deleon", Address = "955 Bel Aire Dr", Grade = 'A' };
+            }
+           return Students;
+        }
+        
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4._1
+{
+    public enum Month
+    {
+        Jan, Feb, Mar,
+        Apr, May, Jun,
+        Jul, Aug, Sep,
+        Oct, Nov, Dec
+
+    }
+    internal class Student
+    {
+       public int StudentId { get; set; }
+       public string FirstName { get; set; }
+       public string LastName { get; set; }
+       public string Address { get; set; }
+       public Month MonthOfAdmission { get; set; }
+       public char Grade { get; set; }
+    }
+}
